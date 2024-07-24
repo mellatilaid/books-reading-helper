ffb91dd61a0b4d909fe164d6953c03eaa167a513
mellatilaid <laidmellati@gmail.com>
2024-07-24 11:54:39 +0200

    created fetch downloaded files cubit method

--- lib/features/home/presentation/manager/fetchdownloadedfiles/fetch_downloaded_files_cubit.dart
+++ lib/features/home/presentation/manager/fetchdownloadedfiles/fetch_downloaded_files_cubit.dart
@@ -3,8 +3,20 @@ import 'dart:io';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
+import '../../../data/repos/home_repo/home_repo_impl.dart';
+
 part 'fetch_downloaded_files_state.dart';
 
 class FetchDownloadedFilesCubit extends Cubit<FetchDownloadedFilesState> {
   FetchDownloadedFilesCubit() : super(FetchDownloadedFilesInitial());
+
+  Future<void> getUserDownloadedPdfs() async {
+    emit(FetchDownloadedFilesLoading());
+    final resault = await HomeRepoImpl().getUserDownloadedPdfs();
+    resault.fold((failure) {
+      emit(FetchDownloadedFilesFailure(failure.errMessage));
+    }, (files) {
+      emit(FetchDownloadedFilesSuccess(files));
+    });
+  }
 }
